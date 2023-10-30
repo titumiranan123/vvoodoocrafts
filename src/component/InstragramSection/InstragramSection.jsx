4f9a5175ad8079404c3786a4f138f0f68a770821
titumiranan123 <titumiranan.gtc@gmail.com>
2023-10-30 14:43:21 +0600

    done

--- src/component/InstragramSection/InstragramSection.jsx
+++ src/component/InstragramSection/InstragramSection.jsx
@@ -33,13 +33,13 @@ const InstragramSection = () => {
     return (
         <div className='mt-10'>
             <section className="fifth_section">
-                <div className="instagram_bar flex justify-between items-center mt-10 mb-10">
+                <div className="instagram_bar flex justify-between items-center flex-col gap-5 md:flex-row mt-10 mb-10">
                     <div className=" flex justify-center items-center text-2xl gap-2">
-                        <FaInstagram className='text-2xl font-semibold' />
+                        <FaInstagram className='text-2xl font-semibold mt-2' />
                         <a className="instra_link text-2xl font-semibold underline" href=""><span>@vvoodoocrafts</span></a>
                     </div>
                     <div>
-                        <div className="btn">Follow on Instagram</div>
+                        <div className="flex gap-2 justify-center items-center bg-[#44484F] text-white py-2 px-4 rounded-lg w-52 ">Follow on Instagram</div>
                     </div>
                 </div>
                 <div className=" ">
